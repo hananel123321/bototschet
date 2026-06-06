1b349face5bc3d6ba4d5c279849981df0038e805
hananel123321 <166995833+hananel123321@users.noreply.github.com>
2026-06-06 17:47:36 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,20 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import Message
+from aiogram import F
+import asyncio
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(F.text == "/start")
+async def start(message: Message):
+    await message.answer("Привет! Я бот-отсчёт ⏳")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
